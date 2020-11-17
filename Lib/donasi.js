f1e31089921c3c05c5c544fb75fd4fe29b5a4c54
Zxuan55 <74528583+Zxuan55@users.noreply.github.com>
2020-11-17 14:38:06 +0800

    Update donasi.js

--- Lib/donasi.js
+++ Lib/donasi.js
@@ -1,5 +1,5 @@
 exports.donasi = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
-	return `🤜🤜{ *MENU DONASI ${Bot_Aero}* }🤛🤛
+	return `🤜🤜{ *MENU DONASI ${Bot-Aero}* }🤛🤛
   
   
 TERIMA KASIH
